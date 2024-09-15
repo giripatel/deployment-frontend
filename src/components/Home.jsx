d9799898184d1ea6cc56d7ebe76243038843bb77
Giridhar <giridharpatel143@gmail.com>
2024-09-16 00:24:37 +0530

    init

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center w-full min-h-screen bg-neutral-200">
+          <div className="rounded-xl shadow-xl bg-gradient-to-tr from-pink-600 via-purple-600 to-red-500 w-1/2 h-96  p-5">
+            <h2 className="text-2xl font-semibold text-center text-white">
+              Yeah It's deployed..........!
+            </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Info.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center w-full min-h-screen bg-neutral-200">
+          <div className="rounded-xl shadow-xl bg-gradient-to-tr from-green-600 via-cyan-600 to-teal-500 w-1/2 h-96  p-5">
+            <h2 className="text-2xl font-semibold text-center text-white">
+              From Info
+            </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Info
